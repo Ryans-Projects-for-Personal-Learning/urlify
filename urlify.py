e355cb5a9b90dcb844018725b4871d91ed1dd4e6
Ryan L <34430126+RyanxLoi@users.noreply.github.com>
2021-02-04 10:12:47 -0500

    Create urlify.py

---
+++ urlify.py
@@ -0,0 +1,48 @@
+def urlify(title):
+    
+    #makes url lower case
+    loweredTitle = title.lower()
+    
+    url = ""
+    
+    unsafeChars = ['"', '#', '$', '%', '&', '+',
+                    ',', '/', ':', ';', '=', '?',
+                    '@', '[', '\\', ']', '^', '`',
+                    '{', '|', '}', '~', "'", "«", "»"]
+    
+    accentChars = {
+        "à": "a",
+        "â": "a", 
+        "ä": "a", 
+        "è": "e", 
+        "é": "e", 
+        "ê": "e", 
+        "ë": "e", 
+        "î": "i", 
+        "ï": "i", 
+        "ô": "o", 
+        "œ": "oe", 
+        "ù": "u", 
+        "û": "u", 
+        "ü": "u", 
+        "ÿ": "y", 
+        "ç": "c"
+        }
+    
+    for char in loweredTitle:
+        if char in unsafeChars:
+            if title.index(char) == len(title)-1:
+                break
+            else:
+                url += "-"
+        elif char in accentChars:
+            url += accentChars.get(char)
+        else:
+            url += char
+    
+    result = url.replace(" ","-")
+    
+    return result
+    
+
+print(urlify("Webinaire: l'histoire ancien de l'États-unis et du bœuf?"))
